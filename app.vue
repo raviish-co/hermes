52a26beec582e70d1555203757d62dfc3f583b6d
manueldembo <madembo18@gmail.com>
2024-01-04 11:39:33 +0100

    feat: adde

--- app.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <h1 class="text-red-500">Hello world!</h1>
-    </div>
-</template>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>.image</div>
+
+    <section>
+        <h1>Guia de Saída de Artigos</h1>
+        <span>.icon</span>
+    </section>
+
+    <section>
+        <form></form>
+    </section>
+
+    <section>
+        <div>
+            <table></table>
+            <button>Adicionar</button>
+        </div>
+        <span>.total</span>
+    </section>
+
+    <div>
+        <button>Solicitar</button>
+        <button>Cancelar</button>
+    </div>
+</template>
