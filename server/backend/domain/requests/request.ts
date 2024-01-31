dbc0172d3901c13fd291219b2a4f453f4544907c
João Santos <josan5368@gmail.com>
2024-01-31 13:50:12 +0100

    refactor: changed name of RequestedItems to Request

---
+++ server/backend/domain/requests/request.ts
@@ -0,0 +1,85 @@
+import { Decimal } from "../../shared/decimal";
+import { Purpose } from "../purposes/purpose";
+import { RequestItem } from "./request_item";
+import { User } from "../user";
+
+type Options = {
+    purpose: Purpose;
+    user: User;
+    returnDate: string;
+};
+
+export enum RequestStatus {
+    PENDING = "Por Devolver",
+}
+
+export class Request {
+    readonly purpose: Purpose;
+    readonly user: User;
+    readonly items: RequestItem[];
+    readonly returnDate: Date;
+    readonly issuedAt: Date;
+    status: RequestStatus;
+    total: Decimal;
+    securityDeposity: Decimal;
+
+    private constructor(purpose: Purpose, user: User, returnDate: Date) {
+        this.purpose = purpose;
+        this.user = user;
+        this.status = RequestStatus.PENDING;
+        this.issuedAt = new Date();
+        this.returnDate = returnDate;
+        this.total = Decimal.fromString("0");
+        this.items = [];
+        this.securityDeposity = Decimal.fromString("0");
+    }
+
+    static create(options: Options): Request {
+        const { purpose, user, returnDate } = options;
+        const returnDateParsed = new Date(returnDate);
+        const requestedArticles = new Request(purpose, user, returnDateParsed);
+        return requestedArticles;
+    }
+
+    addItems(items: RequestItem[]): void {
+        for (const requestLine of items) {
+            this.addItem(requestLine);
+        }
+        this.#calculateSecurityDeposit();
+    }
+
+    addItem(requestedItem: RequestItem): void {
+        const total = requestedItem.getTotal();
+        this.items.push(requestedItem);
+        this.#calculateTotal(total);
+    }
+
+    isSameTotal(total: string): boolean {
+        return this.total.value === total;
+    }
+
+    isSameSecurityDeposit(securityDeposit: string): boolean {
+        return this.securityDeposity.value === securityDeposit;
+    }
+
+    getSecurityDeposit(): Decimal {
+        return this.securityDeposity;
+    }
+
+    getStatus(): string {
+        return this.status;
+    }
+
+    getTotal(): Decimal {
+        return this.total;
+    }
+
+    #calculateTotal(amount: Decimal): void {
+        this.total = this.total.add(amount);
+    }
+
+    #calculateSecurityDeposit(): void {
+        const factor = Decimal.fromString("2");
+        this.securityDeposity = this.total.multiply(factor);
+    }
+}
