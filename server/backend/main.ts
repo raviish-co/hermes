b2e441c30884a60f74edb693c714f6d1633b0871
João Santos <josan5368@gmail.com>
2024-02-02 12:28:36 +0100

    fix: injected dependence of sequence generator

--- server/backend/main.ts
+++ server/backend/main.ts
@@ -1,21 +1,23 @@
-import { ItemService } from "./application/item_service";
-import { RequestService } from "./application/request_service";
-import { JsonPurposeSource } from "./persistense/data/json_purpouse_source";
 import { InmemRequestRepository } from "./persistense/inmem/inmem_request_repository";
+import { InmemSequenceStorage } from "./persistense/inmem/inmem_sequence_storage";
+import { JsonPurposeSource } from "./persistense/data/json_purpouse_source";
+import { SequenceGenerator } from "./domain/sequences/sequence_generator";
 import { ItemRepositoryStub } from "./tests/stubs/item_repository_stub";
-import { StockRepositoryStub } from "./tests/stubs/stock_repository_stub";
+import { RequestService } from "./application/request_service";
+import { ItemService } from "./application/item_service";
 
 const jsonPurposeSource = new JsonPurposeSource();
 const articleRepository = new ItemRepositoryStub();
 const requestRepository = new InmemRequestRepository();
-const stockRepository = new StockRepositoryStub();
+const sequenceStore = new InmemSequenceStorage();
+const sequenceGenerator = new SequenceGenerator(sequenceStore);
 
 export const makeRequestService = (): RequestService => {
     return new RequestService(
         jsonPurposeSource,
         articleRepository,
         requestRepository,
-        stockRepository
+        sequenceGenerator
     );
 };
 
