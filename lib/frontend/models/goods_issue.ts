55ecaa94a551b1f35360bf50b965a7a72e2aa2bb
manueldembo <madembo18@gmail.com>
2024-02-23 12:27:53 +0100

    refactor: use dto

--- lib/frontend/models/goods_issue.ts
+++ lib/frontend/models/goods_issue.ts
@@ -1,21 +1,25 @@
+import type { PurposeSpecification } from "./purpose_specification";
 import type { Condition } from "./condition";
 
-export interface Purpose {
-    description: string;
-    details: string;
-    notes?: string;
+export interface VariationValue {
+    variationId: string;
+    value: string;
 }
 
 export interface GoodsIssueLine {
     itemId: string;
+    name: string;
+    price: string;
     quantity: number;
+    stock: number;
+    total: string;
+    variationsValues?: VariationValue[];
     condition?: Condition;
 }
 
 export interface GoodsIssueModel {
     total: string;
     returnDate: string;
-    securityDeposit: string;
-    purpose: Purpose;
+    purposeSpecification: PurposeSpecification;
     lines: GoodsIssueLine[];
 }
--- lib/frontend/services/goods_issue_service.ts
+++ lib/frontend/services/goods_issue_service.ts
@@ -1,10 +1,47 @@
-import type { GoodsIssueModel } from "@frontend/models/goods_issue";
+import type { GoodsIssueModel, GoodsIssueLine } from "@frontend/models/goods_issue";
+import type { Condition } from "../models/condition";
+import type { PurposeSpecification } from "../models/purpose_specification";
+
+interface Line {
+    itemId: string;
+    quantity: number;
+    condition?: Condition;
+}
+
+interface GoodsIssueDTO {
+    total: string;
+    returnDate: string;
+    purposeSpecification: PurposeSpecification;
+    lines: Line[];
+}
 
 export class GoodsIssueService {
-    async new(request: GoodsIssueModel) {
+    async new(goodsIssue: GoodsIssueModel) {
+        const data = this.#toGoodsIssueDTO(goodsIssue);
+
         return await $fetch("/api/goods-issue", {
             method: "post",
-            body: { request },
+            body: { data },
         });
+    }
+
+    #toGoodsIssueLine(line: GoodsIssueLine): Line {
+        return {
+            itemId: line.itemId,
+            quantity: line.quantity,
+            condition: {
+                comment: line?.condition?.comment,
+                status: line?.condition!.status,
+            },
+        };
+    }
+
+    #toGoodsIssueDTO(goodsIssue: GoodsIssueModel): GoodsIssueDTO {
+        return {
+            total: goodsIssue.total.replace(/\s/g, ""),
+            returnDate: goodsIssue.returnDate,
+            purposeSpecification: goodsIssue.purposeSpecification,
+            lines: goodsIssue.lines.map(this.#toGoodsIssueLine),
+        };
     }
 }
