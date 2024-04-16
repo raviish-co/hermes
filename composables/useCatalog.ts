db56d709cdcdc7e446512d5d779b14dd095f8313
jocaetano1 <josan5368@gmail.com>
2024-04-16 16:50:52 +0100

    feat: view variations in list category page

--- composables/useCatalog.ts
+++ composables/useCatalog.ts
@@ -1,10 +1,15 @@
+import type { CategoryModel } from "~/lib/frontend/models/category";
 import type { ItemModel } from "~/lib/frontend/models/item";
+import type { VariationModel } from "~/lib/frontend/models/variation";
 import { CatalogService } from "~/lib/frontend/services/catalog_service";
 
 const service = new CatalogService();
 
 export function useCatalog() {
     const items = ref<ItemModel[]>([]);
+    const variations = ref<VariationModel[]>([]);
+    const categories = ref<CategoryModel[]>([]);
+
     const pages = ref<number>(1);
 
     const changePage = (page: number) => {
@@ -33,5 +38,31 @@ export function useCatalog() {
         });
     };
 
-    return { items, pages, searchItems, listItems, changePage };
+    const listVariations = () => {
+        service.listVariations().then((res) => {
+            variations.value = res;
+        });
+    };
+
+    const listCategories = () => {
+        service.listCategories().then((res) => {
+            categories.value = res;
+        });
+    };
+
+    const filterVariations = (ids: string[]) => {
+        return variations.value.filter((v) => ids.includes(v.variationId));
+    };
+
+    return {
+        items,
+        pages,
+        categories,
+        searchItems,
+        listItems,
+        listCategories,
+        listVariations,
+        filterVariations,
+        changePage,
+    };
 }
--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
-import { CatalogService } from "~/lib/frontend/services/catalog_service";
-import type { CategoryModel } from "~/lib/frontend/models/category";
-
-const categories = ref<CategoryModel[]>([]);
-
-const service = new CatalogService();
+const catalog = useCatalog();
 
 onMounted(async () => {
-    categories.value = await service.listCategories();
+    catalog.listCategories();
+    catalog.listVariations();
 });
 </script>
 
@@ -18,24 +14,33 @@ onMounted(async () => {
             <table class="table">
                 <thead>
                     <tr>
-                        <th class="min-w-20 w-20">ID</th>
-                        <th class="min-w-60 w-60 text-left">Descrição</th>
-                        <th class="min-w-40 w-40"></th>
-                        <th class="min-w-40 w-40"></th>
-                        <th class="min-w-40 w-40"></th>
+                        <th class="min-w-40 w-40">ID</th>
+                        <th class="min-w-40 w-40 text-left">Nome</th>
+                        <th class="min-w-40 w-40 text-left">Variações</th>
+                        <th class="min-w-40 w-40 text-left">Descrição</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr
-                        v-for="category in categories"
+                        v-for="category in toValue(catalog.categories)"
                         :key="category.categoryId"
                         class="text-center"
                     >
                         <td>{{ category.categoryId }}</td>
                         <td class="text-left">{{ category.name }}</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td class="text-left">
+                            <div class="flex flex-wrap gap-2">
+                                <span
+                                    v-for="variation in catalog.filterVariations(
+                                        category.variationsIds
+                                    )"
+                                    :key="variation.variationId"
+                                >
+                                    <span class="badge-light">{{ variation.name }} <br /></span>
+                                </span>
+                            </div>
+                        </td>
+                        <td class="text-left">{{ category.description }}</td>
                     </tr>
                 </tbody>
             </table>
