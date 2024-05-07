baa301d7bc26542a44e85e98b6b83b926f0219c6
jocaetano1 <josan5368@gmail.com>
2024-05-07 09:31:55 +0100

    refactor: adjustments in goods issue and return services

--- lib/backend/application/goods_issue_service.ts
+++ lib/backend/application/goods_issue_service.ts
@@ -1,21 +1,21 @@
+import type { Generator } from "../adapters/sequences/generator";
+import { Sequence } from "../adapters/sequences/sequence";
+import { InsufficientStock } from "../domain/catalog/items/insufficient_stock_error";
+import { Item } from "../domain/catalog/items/item";
+import type { ItemRepository } from "../domain/catalog/items/item_repository";
+import type { GoodsIssueNote } from "../domain/goods_issue/goods_issue_note";
+import { GoodsIssueNoteBuilder } from "../domain/goods_issue/goods_issue_note_builder";
+import { GoodsIssueNoteLine } from "../domain/goods_issue/goods_issue_note_line";
 import type { GoodsIssueNoteNotFound } from "../domain/goods_issue/goods_issue_note_not_found_error";
 import type { GoodsIssueNoteRepository } from "../domain/goods_issue/goods_issue_note_repository";
-import type { PurposeSpecification } from "../domain/goods_issue/purpose_specification";
-import { GoodsIssueNoteBuilder } from "../domain/goods_issue/goods_issue_note_builder";
-import { InsufficientStock } from "../domain/catalog/items/insufficient_stock_error";
 import { InvalidPurpose } from "../domain/goods_issue/invalid_purpose_error";
-import type { GoodsIssueNote } from "../domain/goods_issue/goods_issue_note";
 import { InvalidTotal } from "../domain/goods_issue/invalid_total_error";
-import { GoodsIssueNoteLine } from "../domain/goods_issue/goods_issue_note_line";
-import type { ItemRepository } from "../domain/catalog/items/item_repository";
-import type { Generator } from "../adapters/sequences/generator";
+import { Purpose } from "../domain/goods_issue/purpose";
+import type { PurposeSpecification } from "../domain/goods_issue/purpose_specification";
+import type { ItemStockRepository } from "../domain/warehouse/item_stock_repository";
 import { type Either, left, right } from "../shared/either";
 import type { GoodsIssueNoteError } from "../shared/errors";
-import { Sequence } from "../adapters/sequences/sequence";
-import { Purpose } from "../domain/goods_issue/purpose";
-import { Item } from "../domain/catalog/items/item";
 import { ID } from "../shared/id";
-import type { ItemStockRepository } from "../domain/warehouse/item_stock_repository";
 
 export class GoodsIssueService {
     #itemRepository: ItemRepository;
--- lib/backend/application/goods_return_service.ts
+++ lib/backend/application/goods_return_service.ts
@@ -1,20 +1,20 @@
-import { GoodsIssueNoteHasBeenReturned } from "../domain/goods_issue/goods_issue_note_has_been_returned_error";
-import { InvalidGoodsIssueLineQuantity } from "../domain/goods_issue/invalid_goods_issue_line_quantity_error";
-import type { GoodsReturnNoteNotFound } from "../domain/goods_return/goods_return_note_not_found_error";
-import type { GoodsReturnNoteRepository } from "../domain/goods_return/goods_return_note_repository";
-import type { GoodsIssueNoteRepository } from "../domain/goods_issue/goods_issue_note_repository";
-import { GoodsIssueLineNotFound } from "../domain/goods_issue/goods_lssue_line_not_found_error";
-import { GoodsReturnNoteLine } from "../domain/goods_return/goods_return_note_line";
-import type { GoodsIssueNoteLine } from "../domain/goods_issue/goods_issue_note_line";
-import type { ItemRepository } from "../domain/catalog/items/item_repository";
-import { GoodsReturnNote } from "../domain/goods_return/goods_return_note";
 import type { Generator } from "../adapters/sequences/generator";
-import type { GoodsReturnNoteError } from "../shared/errors";
-import { left, right, type Either } from "../shared/either";
 import { Sequence } from "../adapters/sequences/sequence";
 import type { Item } from "../domain/catalog/items/item";
-import { ID } from "../shared/id";
+import type { ItemRepository } from "../domain/catalog/items/item_repository";
+import { GoodsIssueNoteHasBeenReturned } from "../domain/goods_issue/goods_issue_note_has_been_returned_error";
+import type { GoodsIssueNoteLine } from "../domain/goods_issue/goods_issue_note_line";
+import type { GoodsIssueNoteRepository } from "../domain/goods_issue/goods_issue_note_repository";
+import { GoodsIssueLineNotFound } from "../domain/goods_issue/goods_lssue_line_not_found_error";
+import { InvalidGoodsIssueLineQuantity } from "../domain/goods_issue/invalid_goods_issue_line_quantity_error";
+import { GoodsReturnNote } from "../domain/goods_return/goods_return_note";
+import { GoodsReturnNoteLine } from "../domain/goods_return/goods_return_note_line";
+import type { GoodsReturnNoteNotFound } from "../domain/goods_return/goods_return_note_not_found_error";
+import type { GoodsReturnNoteRepository } from "../domain/goods_return/goods_return_note_repository";
 import type { ItemStockRepository } from "../domain/warehouse/item_stock_repository";
+import { left, right, type Either } from "../shared/either";
+import type { GoodsReturnNoteError } from "../shared/errors";
+import { ID } from "../shared/id";
 
 export class GoodsReturnService {
     #goodsIssueRepository: GoodsIssueNoteRepository;
@@ -46,28 +46,19 @@ export class GoodsReturnService {
         if (noteOrErr.isLeft()) return left(noteOrErr.value);
 
         const note = noteOrErr.value;
-        const goodsIssueLines = note.lines;
-
         if (note.isReturned()) return left(new GoodsIssueNoteHasBeenReturned());
 
-        const voidOrErr = this.#verifyQuantities(itemsData, goodsIssueLines);
+        const voidOrErr = this.#verifyQuantities(itemsData, note.lines);
         if (voidOrErr.isLeft()) return left(voidOrErr.value);
 
         const itemsIds = itemsData.map((item) => ID.fromString(item.itemId));
         const itemsOrErr = await this.#itemRepository.findAll(itemsIds);
-        const items = <Item[]>itemsOrErr.value;
+        if (itemsOrErr.isLeft()) return left(itemsOrErr.value);
 
-        const itemsStock = await this.#itemStockRepository.findAll(itemsIds);
-
-        for (const stock of itemsStock) {
-            const data = itemsData.find((item) => stock.itemId.equals(ID.fromString(item.itemId)))!;
-            stock.increase(data.quantity);
-        }
-
-        await this.#itemStockRepository.updateAll(itemsStock);
+        await this.#increaseStock(itemsIds, itemsData);
 
         const returnNoteId = this.#buildReturnNoteId();
-        const returnLines = this.#buildReturnLines(itemsData, items);
+        const returnLines = this.#buildReturnLines(itemsData, itemsOrErr.value);
 
         const returnNote = new GoodsReturnNote(
             returnNoteId,
@@ -82,9 +73,9 @@ export class GoodsReturnService {
 
         await this.#goodsReturnRepository.save(returnNote);
 
-        this.#restoreItems(itemsData, items);
+        this.#restoreItems(itemsData, itemsOrErr.value);
 
-        await this.#itemRepository.updateAll(items);
+        await this.#itemRepository.updateAll(itemsOrErr.value);
 
         return right(undefined);
     }
@@ -123,11 +114,21 @@ export class GoodsReturnService {
         return lines;
     }
 
+    async #increaseStock(itemsIds: ID[], itemsData: ItemData[]) {
+        const itemsStock = await this.#itemStockRepository.findAll(itemsIds);
+
+        for (const stock of itemsStock) {
+            const data = itemsData.find((item) => stock.itemId.equals(ID.fromString(item.itemId)))!;
+            stock.increase(data.quantity);
+        }
+
+        this.#itemStockRepository.updateAll(itemsStock);
+    }
+
+    // Apagar isso quando a condição for adicionada a linha de devolução
     #restoreItems(itemsData: ItemData[], items: Item[]): void {
         itemsData.forEach((data) => {
             const item = items.find((item) => item.itemId.equals(ID.fromString(data.itemId)))!;
-
-            item.updateStock(data.quantity);
 
             if (!data.comment) return;
 
