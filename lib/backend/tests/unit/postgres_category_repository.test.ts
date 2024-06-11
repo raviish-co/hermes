2d8dabb644bc10cbde31d6b4bf79eacef61f9ad2
jocaetano1 <josan5368@gmail.com>
2024-06-11 13:12:59 +0100

    test: added test of postgres category repository for findByName method

--- lib/backend/tests/unit/postgres_category_repository.test.ts
+++ lib/backend/tests/unit/postgres_category_repository.test.ts
@@ -7,7 +7,7 @@ import { Variation } from "../../domain/catalog/variations/variation";
 import { PostgresCategoryRepository } from "../../persistense/postgres/postgres_category_repository";
 import { PostgresVariationRepository } from "../../persistense/postgres/postgres_variation_repository";
 
-describe("Postgres Category - getById", () => {
+describe("PostgresCategoryRepostory - getById", () => {
     it("Deve recuperar a categoria pelo id", async () => {
         const categoryId = ID.random();
         const categoryRepository = new PostgresCategoryRepository(prisma);
@@ -48,7 +48,7 @@ describe("Postgres Category - getById", () => {
     });
 });
 
-describe("Postgres Category - getAll", () => {
+describe("PostgresCategoryRepostory - getAll", () => {
     it("Deve recuperar todas as categorias do repsitório", async () => {
         const categoryRepository = new PostgresCategoryRepository(prisma);
 
@@ -56,6 +56,28 @@ describe("Postgres Category - getAll", () => {
 
         expect(categories.length).toBeGreaterThanOrEqual(0);
         expect(categories[0].name).toBe("Roupas");
+    });
+});
+
+describe("PostgresCategoryRepostory - findByName", () => {
+    it("Deve recuperar a categoria pelo nome", async () => {
+        const name = "Roupas";
+        const categoryRepository = new PostgresCategoryRepository(prisma);
+
+        const categoryOrErr = await categoryRepository.findByName(name);
+
+        const category = <Category>categoryOrErr.value;
+        expect(category.name).toEqual(name);
+    });
+
+    it("Deve retornar CategoryNotFound quando a categoria não existe", async () => {
+        const name = "Calças";
+        const categoryRepository = new PostgresCategoryRepository(prisma);
+
+        const categoryOrErr = await categoryRepository.findByName(name);
+
+        expect(categoryOrErr.isLeft()).toBeTruthy();
+        expect(categoryOrErr.value).toBeInstanceOf(CategoryNotFound);
     });
 });
 
