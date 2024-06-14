12d303a7cd029a2ac6a05b928eb31151b372829c
jocaetano1 <josan5368@gmail.com>
2024-06-14 09:25:08 +0100

    feat: added postgres section repository

---
+++ lib/backend/persistense/postgres/postgres_section_repository.ts
@@ -0,0 +1,51 @@
+import type { PrismaClient } from "@prisma/client";
+import { Section } from "../../domain/catalog/departments/section";
+import { SectionNotFound } from "../../domain/catalog/departments/section_not_found_error";
+import type { SectionRepository } from "../../domain/catalog/departments/section_repository";
+import { type Either, left, right } from "../../shared/either";
+import { ID } from "../../shared/id";
+
+function sectionFactory(sectionData: any): Section {
+    return new Section(ID.fromString(sectionData.section_id), sectionData.name, ID.random());
+}
+
+export class PostgresSectionRepository implements SectionRepository {
+    constructor(private prisma: PrismaClient) {}
+
+    async getAll(): Promise<Section[]> {
+        const sectionsData = await this.prisma.section.findMany();
+
+        if (!sectionsData) return [];
+
+        return sectionsData.map(sectionFactory);
+    }
+
+    async findByName(name: string): Promise<Either<SectionNotFound, Section>> {
+        const sectionData = await this.prisma.section.findFirst({
+            where: { name: name },
+        });
+
+        if (!sectionData) return left(new SectionNotFound(name));
+
+        return right(sectionFactory(sectionData));
+    }
+
+    async findById(sectionId: ID): Promise<Either<SectionNotFound, void>> {
+        const sectionData = await this.prisma.section.findUnique({
+            where: { section_id: sectionId.toString() },
+        });
+
+        if (!sectionData) return left(new SectionNotFound(sectionId.toString()));
+
+        return right(undefined);
+    }
+
+    async save(section: Section): Promise<void> {
+        await this.prisma.section.create({
+            data: {
+                section_id: section.sectionId.toString(),
+                name: section.name,
+            },
+        });
+    }
+}
