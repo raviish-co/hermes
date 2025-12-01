9c02c50e4d3464668bec561516b7c1c57c8c2eac
silvano-paulino <silvanocavimbi20@gmail.com>
2025-12-01 06:53:37 +0100

    refactor: added parameters to gmailOtpSender

--- composables/useOtpSender.ts
+++ composables/useOtpSender.ts
@@ -1,8 +1,13 @@
 import { GmailOtpSender } from "~/lib/backend/adapters/senders/gmail_otp_sender";
 import { ConsoleOtpSender } from "../lib/backend/adapters/senders/console_otp_sender";
 
+const clientSecret = process.env.GMAIL_CLIENT_SECRET;
+const clientId = process.env.GMAIL_CLIENT_ID;
+const refreshToken = process.env.GMAIL_CLIENT_REFRESH_TOKEN;
+const fromEmail = process.env.GMAIL_CLIENT_FROM_EMAIL;
 
-const gmailOtpSender = new GmailOtpSender();
+const gmailOtpSender = new GmailOtpSender(clientId!, clientSecret!, fromEmail!, refreshToken!);
 const consoleOtpSender = new ConsoleOtpSender();
 
-export const useOtpSender = () => process.env.NODE_ENV === "production" ? gmailOtpSender : consoleOtpSender;
+export const useOtpSender = () =>
+    process.env.NODE_ENV === "production" ? gmailOtpSender : consoleOtpSender;
