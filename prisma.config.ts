b430fa6158ba5d7c506cfc385f9eaa7620181b55
silvano-paulino <silvanocavimbi20@gmail.com>
2025-12-17 13:45:03 +0100

    chore: upgraded prisma version to solve datasource importing

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from "prisma/config";
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-    schema: "lib/backend/persistence/postgres/prisma/schema.prisma",
+    schema: "server/backend/persistence/postgres/prisma/schema.prisma",
     migrations: {
-        path: "lib/backend/persistence/postgres/prisma/migrations",
+        path: "server/backend/persistence/postgres/prisma/migrations",
+    },
+    datasource: {
+        url: env("NUXT_DATABASE_URL"),
     },
 });
