750eac170ed8d13e427468aab9daaff6552ae2d6
silvano-paulino <silvanocavimbi20@gmail.com>
2025-12-18 15:49:04 +0100

    chore(prisma): resolved prisma client definition

--- app/composables/usePrismaClient.ts
+++ app/composables/usePrismaClient.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const prismaClient = new PrismaClient({ datasourceUrl: useRuntimeConfig().databaseUrl });
+const adapter = new PrismaPg({ connectionString: useRuntimeConfig().databaseUrl });
+const prismaClient = new PrismaClient({ adapter });
 
 export const usePrismaClient = () => prismaClient;
